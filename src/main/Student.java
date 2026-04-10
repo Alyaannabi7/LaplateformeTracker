7ad61f771313a0b39bef1725f95434ff13f41986
yanissaouri <yaniss.aouri@laplateforme.io>
2026-04-10 11:27:43 +0200

    feat/ added student folder

--- src/main/Student.java
+++ src/main/Student.java
@@ -1,5 +1,19 @@
 package main;
 
 public class Student {
-    
+    private String first_name;
+    private String last_name;
+    private int age;
+    private double grade;
+    private String course;
+
+    public Student(String first_name, String last_name, int age, double grade, String course){
+        
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.age = age;
+        this.grade = grade;
+        this.course = course;
+
+    }
 }
